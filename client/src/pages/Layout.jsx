278ad89c2534e110a8320c9911b2f9ee22d335dc
Riya Jogi <riyajogi7@gmail.com>
2026-09-10 20:48:26 +0530

    feat: implement routing with React Router, add Home, Layout, Dashboard, ResumeBuilder, Preview, and Login pages

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <>
-      <div>Layout</div>
+      <div>
+        <h1>Layout Page</h1>
+        <div>
+          <Outlet />
+        </div>
+      </div>
     </>
   )
 }
